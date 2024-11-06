57061b3e46d8e32808403085d40c63853397ec87
Akhmedov76 <ahmadovj5016@gmail.com>
2024-11-06 19:02:46 +0500

    Create BookSerializer

--- app_books/serializers.py
+++ app_books/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from app_books.models import AuthorModel
+from app_books.models import AuthorModel, BookModel
 
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = AuthorModel
+        fields = "__all__"
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BookModel
         fields = "__all__"
