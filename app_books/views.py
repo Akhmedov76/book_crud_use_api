841055cfe1f497b8efecf0f61a4dc485afd07fab
Akhmedov76 <ahmadovj5016@gmail.com>
2024-11-06 18:57:25 +0500

    Create author detail view

--- app_books/views.py
+++ app_books/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from app_books.models import AuthorModel
+from app_books.serializers import AuthorSerializer
 
 
 @api_view(['GET', 'POST'])
@@ -23,3 +24,29 @@ def author_list_create(request):
             return Response(response, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT', 'PATCH', 'DELETE'])
+def author_detail(request, author_id):
+    author = AuthorModel.objects.get(id=author_id)
+    if request.method == ['PUT', 'PATCH']:
+        serializer = AuthorSerializer(author, data=request.data, partial=(request.method == 'PATCH'))
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            response = {
+                'status': True,
+                'message': "Author updated successfully",
+                'data': serializer.data,
+            }
+            return Response(response, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        author.delete()
+        response = {
+            'status': True,
+            'message': "Author deleted successfully",
+        }
+        return Response(response, status=status.HTTP_204_NO_CONTENT)
+    else:
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
