a184effdc7c2cde7a10bd800db6e7705051545dd
Akhmedov76 <ahmadovj5016@gmail.com>
2024-11-06 18:44:41 +0500

    Created books model

--- app_books/models.py
+++ app_books/models.py
@@ -16,3 +16,20 @@ class AuthorModel(models.Model):
         verbose_name = 'author'
         verbose_name_plural = 'authors'
         ordering = ['-id']
+
+
+class BookModel(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(AuthorModel, on_delete=models.CASCADE)
+    publication_date = models.DateField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'book'
+        verbose_name_plural = 'books'
+        ordering = ['-id']
